ffcff331a0b68702ea5e070fafbc9b3f81fcbc1f
Esa Auranen <esa.auranen@iki.fi>
2022-07-05 00:22:29 +0300

    Drop the execution context from AppDb

--- app/models/AppDb.scala
+++ app/models/AppDb.scala
@@ -7,7 +7,7 @@ import play.api.db.Database
 import javax.inject.Inject
 
 
-class AppDb @Inject()(db: Database, ec: DatabaseExecutionContext) {
+class AppDb @Inject()(db: Database) {
 
   val jooq: DSLContext = DSL.using(db.dataSource, SQLDialect.SQLITE)
 
