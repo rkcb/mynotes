317e0f393af6d875ef28c1b6228090a7b646368d
Esa Auranen <esa.auranen@iki.fi>
2022-07-04 16:53:51 +0300

    Modify schema

---
+++ dbmigration/src/main/resources/db/migration/V1__create_theme_table.sql
@@ -0,0 +1,16 @@
+create table theme (
+    name varchar(50) primary key
+);
+
+insert into theme (name)
+values ('scala'),
+       ('java'),
+       ('cli'),
+       ('python'),
+       ('sed'),
+       ('awk'),
+       ('html'),
+       ('css'),
+       ('perl');
+
+
